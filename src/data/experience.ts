6330e23d454950345cf3183cbdaad14cdba38e3a
m0ln1z <anartur476@gmail.com>
2025-08-03 20:07:11 +0500

    new site

---
+++ src/data/experience.ts
@@ -0,0 +1,61 @@
+import type { Props as ExperienceProp } from "../components/experience/props";
+
+export const EXPERIENCE: ExperienceProp[] = [
+  {
+    dates: "December 2024 — Present",
+    title: "Middle Full Stack Engineer",
+    company: "D2D Studio",
+    companyUrl: "https://d2d.media",
+    description:
+      "Developed IOS Application, Chrome Extension, Web-Sites. Settings up CI/CD Pipelines. Backend for Mobile App (Flutter)",
+    technologies: [
+      "FastAPI",
+      "Next",
+      "Vue",
+      "Node",
+      "React",
+      "Python",
+      "Rust",
+      "CI/CD",
+      "Swift",
+      "JavaScript",
+      "Docker",
+      "Java",
+      "Sping MVC",
+      "Spring Boot",
+    ],
+    logo: "/logos/d2d.png",
+  },
+  {
+    dates: "December 2023 — December 2024",
+    title: "Middle Frontend Developer",
+    company: "PRODVIG",
+    companyUrl: "https://prodvig.kz/",
+    description:
+      "Built Next.js applications with server-side rendering optimized for real-time network data visualization and API route processing",
+    technologies: [
+      "Next",
+      "React",
+      "Vue",
+      "Django",
+      "CI/CD",
+      "CRM",
+    ],
+    logo: "/logos/prodvig.jpg",
+  },
+  {
+    dates: "November 2022 — October 2023",
+    title: "Junior Developer",
+    company: "IPCS.KZ",
+    companyUrl: "https://ipcs.kz",
+    description:
+      "Developing Python FastAPI backend services with Django for CRM and client management systems. Integration third-party APIs and payment gateways",
+    technologies: [
+      "Python",
+      "FastAPI",
+      "CRM",
+      "RestAPI",
+    ],
+    logo: "/logos/ipcs.png",
+  },
+];
---
+++ src/data/socials.ts
@@ -0,0 +1,15 @@
+import IconGithub from "../components/icons/github.astro";
+import IconLinkedin from "../components/icons/linkedin.astro";
+
+export const SOCIALS = [
+  {
+    name: "Github",
+    url: "https://github.com/m0ln1z",
+    icon: IconGithub,
+  },
+  {
+    name: "Linkedin",
+    url: "https://www.linkedin.com/in/артур-ан-02200a352/",
+    icon: IconLinkedin,
+  },
+];
